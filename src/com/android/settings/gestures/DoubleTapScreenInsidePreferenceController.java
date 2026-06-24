FortuneOS-CAF/platform_packages_apps_Settings

Settings: Rework DT2W settings to match recent ST2W and PU2W changes
Change-Id: Idbf6396aadb4df4b39df35a225b821f091a51d26
Signed-off-by: Kizziama <kizziama@proton.me>

src/org/fortune/settings/gestures/DoubleTapScreenInsidePreferenceController.java
@@ -1,0 +1,82 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Yet Another AOSP Project
+ * SPDX-FileCopyrightText: 2024 Paranoid Android
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.fortune.settings.gestures;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.provider.Settings;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import org.fortune.framework.preferences.SecureSettingSwitchPreference;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.widget.MainSwitchPreference;
+
+public class DoubleTapScreenInsidePreferenceController extends AbstractPreferenceController
+        implements PreferenceControllerMixin, OnCheckedChangeListener {
+
+    private static final String KEY = "gesture_double_tap_screen";
+
+    private final Context mContext;
+    private MainSwitchPreference mSwitch;
+
+    public DoubleTapScreenInsidePreferenceController(Context context) {
+        super(context);
+        mContext = context;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mSwitch = screen.findPreference(getPreferenceKey());
+        mSwitch.setOnPreferenceClickListener(preference -> {
+            final boolean enabled = Settings.Secure.getInt(mContext.getContentResolver(),
+                    Settings.Secure.DOZE_DOUBLE_TAP_GESTURE, 1) == 1;
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                    Settings.Secure.DOZE_DOUBLE_TAP_GESTURE,
+                    enabled ? 0 : 1);
+            return true;
+        });
+        mSwitch.addOnSwitchChangeListener(this);
+        updateState(mSwitch);
+    }
+
+    public void setChecked(boolean isChecked) {
+        if (mSwitch != null) {
+            mSwitch.updateStatus(isChecked);
+        }
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        final boolean enabled = Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.DOZE_DOUBLE_TAP_GESTURE, 1) == 1;
+        setChecked(enabled);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        Settings.Secure.putInt(mContext.getContentResolver(),
+                Settings.Secure.DOZE_DOUBLE_TAP_GESTURE, isChecked ? 1 : 0);
+    }
+}

src/com/android/settings/gestures/DoubleTapScreenSettings.java
@@ -21,12 +21,19 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.hardware.display.AmbientDisplayConfiguration;
 
+import org.fortune.settings.gestures.DoubleTapScreenInsidePreferenceController;
+
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.dashboard.suggestions.SuggestionFeatureProvider;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.search.SearchIndexable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SearchIndexable
 public class DoubleTapScreenSettings extends DashboardFragment {
@@ -43,9 +50,18 @@ public class DoubleTapScreenSettings extends DashboardFragment {
                 FeatureFactory.getFeatureFactory().getSuggestionFeatureProvider();
         SharedPreferences prefs = suggestionFeatureProvider.getSharedPrefs(context);
         prefs.edit().putBoolean(PREF_KEY_SUGGESTION_COMPLETE, true).apply();
+    }
 
-        use(DoubleTapScreenPreferenceController.class)
-                .setConfig(new AmbientDisplayConfiguration(context));
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getSettingsLifecycle());
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(Context context,
+            Lifecycle lifecycle) {
+        List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new DoubleTapScreenInsidePreferenceController(context));
+        return controllers;
     }
 
     @Override
@@ -60,7 +76,7 @@ public class DoubleTapScreenSettings extends DashboardFragment {
 
     @Override
     protected int getPreferenceScreenResId() {
-        return R.xml.double_tap_screen_settings;
+        return R.xml.double_tap_screen_settings_doze;
     }
 
     @Override
@@ -69,5 +85,5 @@ public class DoubleTapScreenSettings extends DashboardFragment {
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider(R.xml.double_tap_screen_settings);
+            new BaseSearchIndexProvider(R.xml.double_tap_screen_settings_doze);
 }

neoteric-shadedark/android_packages_apps_Settings

src/com/android/settings/gestures/DoubleTapScreenInsidePreferenceController.java
@@ -1,0 +1,82 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Yet Another AOSP Project
+ * SPDX-FileCopyrightText: 2024 Paranoid Android
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.gestures;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.provider.Settings;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import org.neoteric.preference.SecureSettingSwitchPreference;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.widget.MainSwitchPreference;
+
+public class DoubleTapScreenInsidePreferenceController extends AbstractPreferenceController
+        implements PreferenceControllerMixin, OnCheckedChangeListener {
+
+    private static final String KEY = "gesture_double_tap_screen";
+
+    private final Context mContext;
+    private MainSwitchPreference mSwitch;
+
+    public DoubleTapScreenInsidePreferenceController(Context context) {
+        super(context);
+        mContext = context;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mSwitch = screen.findPreference(getPreferenceKey());
+        mSwitch.setOnPreferenceClickListener(preference -> {
+            final boolean enabled = Settings.Secure.getInt(mContext.getContentResolver(),
+                    Settings.Secure.DOZE_DOUBLE_TAP_GESTURE, 1) == 1;
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                    Settings.Secure.DOZE_DOUBLE_TAP_GESTURE,
+                    enabled ? 0 : 1);
+            return true;
+        });
+        mSwitch.addOnSwitchChangeListener(this);
+        updateState(mSwitch);
+    }
+
+    public void setChecked(boolean isChecked) {
+        if (mSwitch != null) {
+            mSwitch.updateStatus(isChecked);
+        }
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        final boolean enabled = Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.DOZE_DOUBLE_TAP_GESTURE, 1) == 1;
+        setChecked(enabled);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        Settings.Secure.putInt(mContext.getContentResolver(),
+                Settings.Secure.DOZE_DOUBLE_TAP_GESTURE, isChecked ? 1 : 0);
+    }
+}

src/com/android/settings/gestures/DoubleTapScreenSettings.java
@@ -21,12 +21,19 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.hardware.display.AmbientDisplayConfiguration;
 
+import com.android.settings.gestures.DoubleTapScreenInsidePreferenceController;
+
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.dashboard.suggestions.SuggestionFeatureProvider;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.search.SearchIndexable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SearchIndexable
 public class DoubleTapScreenSettings extends DashboardFragment {
@@ -43,9 +50,18 @@ public class DoubleTapScreenSettings extends DashboardFragment {
                 FeatureFactory.getFeatureFactory().getSuggestionFeatureProvider();
         SharedPreferences prefs = suggestionFeatureProvider.getSharedPrefs(context);
         prefs.edit().putBoolean(PREF_KEY_SUGGESTION_COMPLETE, true).apply();
+    }
 
-        use(DoubleTapScreenPreferenceController.class)
-                .setConfig(new AmbientDisplayConfiguration(context));
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getSettingsLifecycle());
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(Context context,
+            Lifecycle lifecycle) {
+        List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new DoubleTapScreenInsidePreferenceController(context));
+        return controllers;
     }
 
     @Override
@@ -60,7 +76,7 @@ public class DoubleTapScreenSettings extends DashboardFragment {
 
     @Override
     protected int getPreferenceScreenResId() {
-        return R.xml.double_tap_screen_settings;
+        return R.xml.double_tap_screen_settings_doze;
     }
 
     @Override
@@ -69,5 +85,5 @@ public class DoubleTapScreenSettings extends DashboardFragment {
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider(R.xml.double_tap_screen_settings);
+            new BaseSearchIndexProvider(R.xml.double_tap_screen_settings_doze);
 }