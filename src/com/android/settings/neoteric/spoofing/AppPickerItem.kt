GenesisOS/packages_apps_Settings

[SQUASH] Settings: Add spoofing options for PIF, TrickyStore and game props
Settings: Add pref screens for game spoof, PIF and trickstore
Ref:
https://github.com/AxionAOSP/android_packages_apps_AxionParts/blob/lineage-23.2/src/com/android/axion/axionparts/ui/screens/GameSpoofingScreen.kt
https://github.com/AxionAOSP/android_packages_apps_AxionParts/blob/lineage-23.2/src/com/android/axion/axionparts/ui/screens/PlayIntegrityFixScreen.kt
https://github.com/AxionAOSP/android_packages_apps_AxionParts/blob/lineage-23.2/src/com/android/axion/axionparts/ui/screens/TrickyStoreScreen.kt

Notes:
* Complete rewrite from scratch to use SettingsPreferenceFragment without compose code.
* Reworked app list for TrickyStore to show relevant system apps + user apps.
* Reworked PIF fetching logic to let user select device and fix fetching latest version.

Settings: Add steps for PIF and TrickyStore

Settings: Move trickystore, pif and game props spoof

Settings: Fix info preferences clipping

Settings: TrickyStore: restore target mode from disk on fragment resume

ListPreference value is reset to its XML defaultValue ("auto") every
time the fragment is recreated on back-navigation. Fix this by reading
the suffix of the first line in target.txt and setting the preference
value accordingly in refreshStatus(), making the file the single source
of truth instead of SharedPreferences.

'!' suffix → cert
'?' suffix → leaf
no suffix / missing file → auto

Settings: Fixing spoofing structure

Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

Ref: AxionAOSP/android_packages_apps_AxionParts@8a6a6bc

Settings: Check for all versions when parsing canary releases

Settings: Add auto-select button for tricky store apps list

Settings: Kill photos app on toggling spoofing

Settings: Add separate tunable for gphotos spoof

Settings: spoof: Read settings from current user

Settings: pif: Enable photos spoof by default

Settings: Add app picker screen

src/com/android/settings/genesis/spoofing/AppPickerItem.kt
@@ -1,0 +1,139 @@
+/*
+ * SPDX-FileCopyrightText: Evolution X
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.genesis.spoofing
+
+import android.graphics.drawable.Drawable
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Badge
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.core.graphics.drawable.toBitmap
+import com.android.settings.R
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun AppPickerItem(
+    packageName: String,
+    label: String,
+    icon: Drawable?,
+    isSystem: Boolean,
+    checked: Boolean,
+    enabled: Boolean = true,
+    onToggle: (Boolean) -> Unit,
+    extraContent: @Composable (() -> Unit)? = null,
+) {
+    val iconBitmap = remember(packageName) {
+        runCatching { icon?.toBitmap(96, 96)?.asImageBitmap() }.getOrNull()
+    }
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .animateContentSize(
+                animationSpec = MaterialTheme.motionScheme.defaultSpatialSpec()
+            ),
+        shape = RoundedCornerShape(14.dp),
+        color = if (checked)
+            MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.25f)
+        else
+            MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.45f),
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable(enabled = enabled) { onToggle(!checked) },
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                if (iconBitmap != null) {
+                    Image(
+                        bitmap = iconBitmap,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(40.dp)
+                            .clip(RoundedCornerShape(10.dp)),
+                    )
+                } else {
+                    Box(
+                        modifier = Modifier
+                            .size(40.dp)
+                            .clip(RoundedCornerShape(10.dp))
+                            .background(MaterialTheme.colorScheme.surfaceVariant),
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(12.dp))
+
+                Column(modifier = Modifier.weight(1f)) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(
+                            text = label,
+                            style = MaterialTheme.typography.bodyLarge,
+                            fontWeight = FontWeight.SemiBold,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            modifier = Modifier.weight(1f, fill = false),
+                        )
+                        if (isSystem) {
+                            Spacer(modifier = Modifier.width(6.dp))
+                            Badge(
+                                containerColor = MaterialTheme.colorScheme.tertiary,
+                                contentColor = MaterialTheme.colorScheme.onTertiary,
+                            ) { Text(stringResource(R.string.system_badge)) }
+                        }
+                    }
+                    Text(
+                        text = packageName,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Switch(
+                    checked = checked,
+                    onCheckedChange = onToggle,
+                    enabled = enabled,
+                )
+            }
+
+            extraContent?.invoke()
+        }
+    }
+}

src/com/android/settings/genesis/spoofing/AppPickerSearchField.kt
@@ -1,0 +1,49 @@
+/*
+ * SPDX-FileCopyrightText: Evolution X
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.genesis.spoofing
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.android.settings.R
+
+@Composable
+fun AppPickerSearchField(
+    query: String,
+    onQueryChange: (String) -> Unit,
+    enabled: Boolean = true,
+) {
+    OutlinedTextField(
+        value = query,
+        onValueChange = onQueryChange,
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(stringResource(R.string.search_apps)) },
+        leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+        trailingIcon = {
+            if (query.isNotEmpty()) {
+                IconButton(onClick = { onQueryChange("") }) {
+                    Icon(
+                        Icons.Default.Close,
+                        contentDescription = stringResource(R.string.clear_search),
+                    )
+                }
+            }
+        },
+        singleLine = true,
+        shape = RoundedCornerShape(12.dp),
+        enabled = enabled,
+    )
+}

neoteric-shadedark/android_packages_apps_Settings

Signed-off-by: ShadeDark6 <nizamfa6@gmail.com>

src/com/android/settings/neoteric/spoofing/AppPickerItem.kt
@@ -1,0 +1,139 @@
+/*
+ * SPDX-FileCopyrightText: Evolution X
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.neoteric.spoofing
+
+import android.graphics.drawable.Drawable
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Badge
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.core.graphics.drawable.toBitmap
+import com.android.settings.R
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun AppPickerItem(
+    packageName: String,
+    label: String,
+    icon: Drawable?,
+    isSystem: Boolean,
+    checked: Boolean,
+    enabled: Boolean = true,
+    onToggle: (Boolean) -> Unit,
+    extraContent: @Composable (() -> Unit)? = null,
+) {
+    val iconBitmap = remember(packageName) {
+        runCatching { icon?.toBitmap(96, 96)?.asImageBitmap() }.getOrNull()
+    }
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .animateContentSize(
+                animationSpec = MaterialTheme.motionScheme.defaultSpatialSpec()
+            ),
+        shape = RoundedCornerShape(14.dp),
+        color = if (checked)
+            MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.25f)
+        else
+            MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.45f),
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable(enabled = enabled) { onToggle(!checked) },
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                if (iconBitmap != null) {
+                    Image(
+                        bitmap = iconBitmap,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(40.dp)
+                            .clip(RoundedCornerShape(10.dp)),
+                    )
+                } else {
+                    Box(
+                        modifier = Modifier
+                            .size(40.dp)
+                            .clip(RoundedCornerShape(10.dp))
+                            .background(MaterialTheme.colorScheme.surfaceVariant),
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(12.dp))
+
+                Column(modifier = Modifier.weight(1f)) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(
+                            text = label,
+                            style = MaterialTheme.typography.bodyLarge,
+                            fontWeight = FontWeight.SemiBold,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            modifier = Modifier.weight(1f, fill = false),
+                        )
+                        if (isSystem) {
+                            Spacer(modifier = Modifier.width(6.dp))
+                            Badge(
+                                containerColor = MaterialTheme.colorScheme.tertiary,
+                                contentColor = MaterialTheme.colorScheme.onTertiary,
+                            ) { Text(stringResource(R.string.system_badge)) }
+                        }
+                    }
+                    Text(
+                        text = packageName,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Switch(
+                    checked = checked,
+                    onCheckedChange = onToggle,
+                    enabled = enabled,
+                )
+            }
+
+            extraContent?.invoke()
+        }
+    }
+}

src/com/android/settings/neoteric/spoofing/AppPickerSearchField.kt
@@ -1,0 +1,49 @@
+/*
+ * SPDX-FileCopyrightText: Evolution X
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.neoteric.spoofing
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.android.settings.R
+
+@Composable
+fun AppPickerSearchField(
+    query: String,
+    onQueryChange: (String) -> Unit,
+    enabled: Boolean = true,
+) {
+    OutlinedTextField(
+        value = query,
+        onValueChange = onQueryChange,
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(stringResource(R.string.search_apps)) },
+        leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+        trailingIcon = {
+            if (query.isNotEmpty()) {
+                IconButton(onClick = { onQueryChange("") }) {
+                    Icon(
+                        Icons.Default.Close,
+                        contentDescription = stringResource(R.string.clear_search),
+                    )
+                }
+            }
+        },
+        singleLine = true,
+        shape = RoundedCornerShape(12.dp),
+        enabled = enabled,
+    )
+}